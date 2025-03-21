6a189b156605261be18a31c542c7b0397faf707a
Jeffrey Flores <Jflores62@gmail.com>
2025-03-21 12:50:01 -0400

    Module 3: Dynamic Trips Rendering With Handlebars and JSON

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,4 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports.travel = function(req, res) {
-    res.render('travel', { title: 'Travel Destinations' });
-  };
-  
+  const dataPath = path.join(__dirname, '../../data/trips.json');
+  const trips = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+
+  res.render('travel', {
+    title: 'Travel Destinations',
+    trips
+  });
+};
